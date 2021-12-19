3b012cb3b57d4dd7900edd7232186f5901a31698
Joseph Li <13826851+joseph-x-li@users.noreply.github.com>
2021-12-18 23:06:15 -0800

    test

--- camera/servo.py
+++ camera/servo.py
@@ -1,18 +1,17 @@
 from adafruit_servokit import ServoKit
-
-
-
+import time
 
 class ServoArray:
-    def __init__(self):
-       pass 
+  reset_angle = [120, 120, 60, 60]
+  def __init__(self):
+    self.kit = ServoKit(channels=16)
+  
+  def reset(self):
+    for i in range(4):
+      self.kit.servo[i].angle = self.reset_angle[i]
+    time.sleep(0.05)
+    for i in range(4):
+      self.kit.servo[i].angle = None
 
 
-kit = ServoKit(channels=16)
-kit.servo[0].angle = 0
-
-import time; time.sleep(1)
-kit.servo[1].angle = 120
-time.sleep(1)
-kit.servo[0].angle = None
-time.sleep(0.5)
+  def 
