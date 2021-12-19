b8304757c74f5ffb5706079c1376a046bf6b528d
Joseph Li <13826851+joseph-x-li@users.noreply.github.com>
2021-12-19 16:45:32 -0500

    hehexc

--- camera/servo.py
+++ camera/servo.py
@@ -2,18 +2,46 @@ from adafruit_servokit import ServoKit
 import time
 
 class ServoArray:
-  reset_angle = [120, 120, 60, 60]
+  reset_angle = [120, 120, 40, 40]
+  power_angle = [180, 180, 90, 90]
+  battery_angle = [50, 50, 0, 0]
+  
   def __init__(self):
     self.kit = ServoKit(channels=16)
   
   def reset(self):
     for i in range(4):
       self.kit.servo[i].angle = self.reset_angle[i]
+      time.sleep(0.20)
+    time.sleep(0.20)
+    for i in range(4):
+      self.kit.servo[i].angle = None
+  
+  def power_position(self):
+    for i in range(4):
+      self.kit.servo[i].angle = self.power_angle[i]
+      time.sleep(0.20)
+    time.sleep(0.20)
+    for i in range(4):
+      self.kit.servo[i].angle = None
+
+  def battery_position(self):
+    for i in range(4):
+      self.kit.servo[i].angle = self.battery_angle[i]
+      time.sleep(0.20)
     time.sleep(0.20)
     for i in range(4):
       self.kit.servo[i].angle = None
 
 
+
 if __name__ == "__main__":
   servo = ServoArray()
+  input()
+  servo.power_position()
+  input()
+  servo.reset()
+  input()
+  servo.battery_position()
+  input()
   servo.reset()
